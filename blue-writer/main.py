ba7eed78b4696dedf75e2378bc955338e343863c
Daniel Lam <lamdanielalex@gmail.com>
2024-12-24 23:23:39 -0500

    Made writer a flask service, works on orb

--- blue-writer/main.py
+++ blue-writer/main.py
@@ -1,18 +1,26 @@
-import time
+from flask import Flask
+from flask import request
 
-def main():
-    print("Hello, world!")
+app = Flask(__name__)
 
-    while True:
-        print("Hi, I'm still here!")
-        time.sleep(1)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-        with open("data/data.txt", "w") as file:
-            log = f"write file! {time.ctime()}"
-            file.write(log)
-            print(log)
+@app.route('/write', methods=['POST'])
+def post_string():
+    
+    if request.is_json:
+        data = request.get_json()
+    elif request.form:
+        data = request.form
+    else:
+        data = request.get_data().decode()
+    
+    with open("data/data.txt", "w") as file:
+        file.write(data)
 
-       
+    return f"Received data: {data}", 200
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
