84d331df75d25c97454a080f1bf5ee35c4b24190
Daniel Lam <lamdanielalex@gmail.com>
2024-12-26 23:55:40 -0500

    Add end to end test

--- blue-writer/main.py
+++ blue-writer/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/write', methods=['POST'])
+@app.route('/write', methods=['PUT'])
 def post_string():
     
     if request.is_json:
--- test.sh
+++ test.sh
@@ -9,4 +9,13 @@ docker build -t blue-writer blue-writer
 kubectl apply -f blue.yaml
 
 # wait for the new pod to be spun up
+kubectl rollout restart deployment/blue-app
 kubectl rollout status deployment/blue-app
+
+# Read the old value
+curl localhost:81/read
+
+# HTTP Put a new value
+curl -X PUT -d "new value" -H "Content-Type: text/plain" localhost:80/write
+
+curl localhost:81/read
